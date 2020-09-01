aac7c05b975f28a87740d77e9e5080184048fdec
sitongju <56384750+sitongju@users.noreply.github.com>
2020-09-01 22:03:41 +0800

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Name: Sitong Ju
+# Email: sitongju@usc.edu
+
+# In[2]:
+
+
+def printsth(word):
+    print(word)
+
+
+# How to invoke lab1.py:
+# printsth("Welcome to DSCI 510!")
+# or enter any string in the braket
+# 
+# Sample output:
+
+# In[3]:
+
+
+printsth("Welcome to DSCI 510!")
+
+
+# In[ ]:
+
+
+
+
---
+++ Readme.md
@@ -0,0 +1,27 @@
+Name: Sitong Ju
+Email: sitongju@usc.edu
+
+
+```python
+def printsth(word):
+    print(word)
+```
+
+How to invoke lab1.py:
+printsth("Welcome to DSCI 510!")
+or enter any string in the braket
+
+Sample output:
+
+
+```python
+printsth("Welcome to DSCI 510!")
+```
+
+    Welcome to DSCI 510!
+
+
+
+```python
+
+```
